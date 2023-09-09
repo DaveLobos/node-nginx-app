75af4018004065d3cd1fdd3c7359604f7c967563
Dave Lobos <dave@davelobos.dev>
2023-09-09 10:18:44 -0600

    added css

--- node/app/main.js
+++ node/app/main.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
+const page = `
+<html>
+<head>
+<title>It works!</title>
+<link rel="stylesheet" type="text/css" href="/css/style.css" />
+</head>
+<body>It works!</body>
+</html>
+`;
+
 http.createServer((req, res) => {
   res.statusCode = 404;
-  res.end("It works!");
+  res.end(page);
 })
   .listen(3000, "0.0.0.0");
