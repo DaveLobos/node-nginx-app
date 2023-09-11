13a637a6679ce57d8d1c6461def8f425f800851c
Dave Lobos <63807394+DaveLobos@users.noreply.github.com>
2023-09-10 18:14:15 -0600

    Update expose.sh

--- bash/expose.sh
+++ bash/expose.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Script to expose the deployment as a load balancer on port 8080
 
 kubectl expose deployment app --type=LoadBalancer --port=8080 --name http-server
