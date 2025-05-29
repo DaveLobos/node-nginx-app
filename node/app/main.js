62bcfd0c353f31f413062bed55d20fd7d6b00782
Dave Lobos <davelobos@pm.me>
2025-05-28 22:16:58 -0600

    improved how static files are served

--- node/app/main.js
+++ node/app/main.js
@@ -3,10 +3,10 @@ const http = require('http');
 const html = `
 <html>
 <head>
-  <title>It works!</title>
-  <link rel="stylesheet" type="text/css" href="/css/style.css" />
+  <title>Hola Mundo</title>
+  <link rel="stylesheet" type="text/css" href="/static/styles.css" />
 </head>
-<body>It works!</body>
+<body>Hola Mundo</body>
 </html>
 `;
 
@@ -14,4 +14,4 @@ http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(html);
 })
-.listen(3000, "0.0.0.0");
+  .listen(3000, "0.0.0.0");
